c933642772eec11569b5490547edf201455947e5
EveHaddox <143825195+EveHaddox@users.noreply.github.com>
2025-03-19 22:54:37 +0100

    font scaling

--- lua/eve/misc/font.lua
+++ lua/eve/misc/font.lua
@@ -1,8 +1,23 @@
 function eve.CreateFont(name, size, weight)
+    eve.scaledFonts = eve.scaledFonts or {}
+
     surface.CreateFont("eve." .. name, {
         font = "Montserrat SemiBold",
         size = size or 16,
         weight = weight or 500
     })
+
+    eve.scaledFonts[name] = {
+        font = "Montserrat SemiBold",
+        size = size or 16,
+        weight = weight or 500
+    }
+
+    hook.Add("OnScreenSizeChanged", "Eve.ReRegisterFonts", function()
+        for k,v in pairs(eve.scaledFonts) do
+            Elib.RegisterFont("eve." .. k, v.font, v.size, v.weight)
+        end
+    end)
+
     return ("eve." .. name)
-end
+end 
